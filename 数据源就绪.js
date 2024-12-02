f19e723a0ba07c604a0f652601c3682d312a6ac0
zhangcongcong <66811734+sorryzcc@users.noreply.github.com>
2024-12-02 10:35:54 +0800

    feat: 1、新增数据源就绪.js

---
+++ 数据源就绪.js
@@ -0,0 +1,5 @@
+state.key1 = data.ClientText.formData.InGameKey.split('_')[0];
+state.key2 = data.ClientText.formData.InGameKey.split('_')[1];
+state.key3 = data.ClientText.formData.InGameKey.split('_')[2];
+state.where1 = 'mainInterface'
+state.where2 = data.ClientText.formData.Context
