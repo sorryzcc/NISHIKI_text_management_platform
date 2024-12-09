f3555485fe506f5de8ab71476b75c3eacb2311ef
zhangcongcong <66811734+sorryzcc@users.noreply.github.com>
2024-12-09 17:31:02 +0800

    原始js

--- 宝可梦模板新建_2_批量调用数据源create接口(批量调用数据源接口).js
+++ 宝可梦模板新建_2_批量调用数据源create接口(批量调用数据源接口).js
@@ -1,26 +1,26 @@
 {
-    // 将需要批量的请求参数填入params中
-    // url参数
-    query:{
-      params: []
-    },
-    // post包体
-    body:{
-      params:[
-  {Version:state.version,Origin:state.pokemonInput1,Context:state.pokemonInput1,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Name_'},
-  {Version:state.version,Origin:state.pokemonInput2,Context:state.pokemonInput2,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Desc_'},
-  {Version:state.version,Origin:state.pokemonInput3,Context:state.pokemonInput3,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Name_'},
-  {Version:state.version,Origin:state.pokemonInput4,Context:state.pokemonInput4,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Desc_'},
-  {Version:state.version,Origin:state.pokemonInput5,Context:state.pokemonInput5,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Name_'},
-  {Version:state.version,Origin:state.pokemonInput6,Context:state.pokemonInput6,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Desc_'},
-  {Version:state.version,Origin:state.pokemonInput7,Context:state.pokemonInput7,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Name_'},
-  {Version:state.version,Origin:state.pokemonInput8,Context:state.pokemonInput8,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Desc_'},
-  {Version:state.version,Origin:state.pokemonInput9,Context:state.pokemonInput9,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Name_'},
-  {Version:state.version,Origin:state.pokemonInput10,Context:state.pokemonInput10,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Desc_'},
-  {Version:state.version,Origin:state.pokemonInput11,Context:state.pokemonInput11,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Name_'},
-  {Version:state.version,Origin:state.pokemonInput12,Context:state.pokemonInput12,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Desc_'},
-  {Version:state.version,Origin:state.pokemonInput13,Context:state.pokemonInput13,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Name_'},
-  {Version:state.version,Origin:state.pokemonInput14,Context:state.pokemonInput14,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Desc_'}
-  ]
-    }
+  // 将需要批量的请求参数填入params中
+  // url参数
+  query:{
+    params: []
+  },
+  // post包体
+  body:{
+    params:[
+{Version:state.version,Origin:state.pokemonInput1,Context:state.pokemonInput1,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Name_'},
+{Version:state.version,Origin:state.pokemonInput2,Context:state.pokemonInput2,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Desc_'},
+{Version:state.version,Origin:state.pokemonInput3,Context:state.pokemonInput3,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Name_'},
+{Version:state.version,Origin:state.pokemonInput4,Context:state.pokemonInput4,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Desc_'},
+{Version:state.version,Origin:state.pokemonInput5,Context:state.pokemonInput5,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Name_'},
+{Version:state.version,Origin:state.pokemonInput6,Context:state.pokemonInput6,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Desc_'},
+{Version:state.version,Origin:state.pokemonInput7,Context:state.pokemonInput7,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Name_'},
+{Version:state.version,Origin:state.pokemonInput8,Context:state.pokemonInput8,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Desc_'},
+{Version:state.version,Origin:state.pokemonInput9,Context:state.pokemonInput9,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Name_'},
+{Version:state.version,Origin:state.pokemonInput10,Context:state.pokemonInput10,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Desc_'},
+{Version:state.version,Origin:state.pokemonInput11,Context:state.pokemonInput11,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Name_'},
+{Version:state.version,Origin:state.pokemonInput12,Context:state.pokemonInput12,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Desc_'},
+{Version:state.version,Origin:state.pokemonInput13,Context:state.pokemonInput13,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Name_'},
+{Version:state.version,Origin:state.pokemonInput14,Context:state.pokemonInput14,Used:'Normal',Platform:'All Platforms',InGameKey:'Pkm_Skill_Desc_'}
+]
   }
+}
