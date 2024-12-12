5c0b12adac315fb38285eb77eedc35349063b511
zhangcongcong <66811734+sorryzcc@users.noreply.github.com>
2024-12-12 09:15:03 +0800

    feat:修改SQL_DB.sql

--- sqlDB.sql
+++ sqlDB.sql
@@ -1,4 +1,8 @@
-INSERT INTO ClientText (ID, TextChangeTag)
+INSERT INTO ClientText (ID, TextChangeTag,Version,Context,Used,Platform),
 VALUES {{values}}
 ON DUPLICATE KEY UPDATE
+Version = VALUES(Version),
+Context = VALUES(Context),
+Used = VALUES(Used),
+Platform = VALUES(Platform),
 TextChangeTag = VALUES(TextChangeTag);
