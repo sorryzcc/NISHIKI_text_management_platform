a024bcac9a0f6a0d9387dfb4bebc3ce6cd1b6b2e
zhangcongcong <66811734+sorryzcc@users.noreply.github.com>
2025-01-20 11:13:05 +0800

    检查originstr这个字符串是否包含whitelist数组里任意一个字符串

--- 本地/白名单优先.js
+++ 本地/白名单优先.js
@@ -2,6 +2,7 @@ const blacklist = ['参赛证'];
 const whitelist = ['集结参赛证'];
 const originStr = '这是可变更宝可梦集结参赛证卡面的道具。购买后可以在宝可梦一览界面中变更想要显示的卡面。';
 
+// 检查originstr这个字符串是否包含whitelist数组里任意一个字符串
 // 检查是否包含任何白名单中的完整词语
 const containsWhitelistWord = whitelist.some(word => originStr.includes(word));
 
